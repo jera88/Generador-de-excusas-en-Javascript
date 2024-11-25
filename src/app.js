13e494efd9515a4a9b83a0167615a8dca1f8c65d
jera88 <117536039+jera88@users.noreply.github.com>
2024-11-25 01:24:56 +0000

    proyecto 7

--- src/app.js
+++ src/app.js
@@ -6,6 +6,29 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let text = "";
+  let excuse = document.getElementById("excuse");
+
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying",
+  ];
+
+  text += who[Math.trunc(Math.random() * 3)] + " ";
+  text += action[Math.trunc(Math.random() * 3)] + " ";
+  text += what[Math.trunc(Math.random() * 3)] + " ";
+  text += when[Math.trunc(Math.random() * 4)];
+
+  excuse.textContent = text;
+
+  excuse.style.fontSize = "50px";
+  excuse.style;
+
+  console.log(text);
 };
